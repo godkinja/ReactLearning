210d9d4b289d10671687e3beadeaa9a64b10db0b
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-21 22:21:58 +0100

    Introduction to JSX

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 ReactDOM.render(<h1 className="header">Hello, Everyone!</h1>, document.getElementById('root')) // 1º param: o que será renderizado, 2º param: onde será renderizado
 
 ReactDOM.render(
@@ -23,3 +24,44 @@ const h1 = document.createElement('h1')
 h1.textContent = 'This is a text from JS'
 h1.className = 'header'
 document.getElementById('js').appendChild(h1)
+*/
+
+// JSX
+
+/*
+const page = (
+
+    <div>
+
+        <h1 className="header">This is JSX</h1> 
+
+        <p>This is a paragraph</p>
+
+    </div>
+)
+*/
+
+const navbar = (
+
+    <nav>
+
+        <h1>Scan'N'Go</h1>
+
+        <ul>
+
+            <li>Pricing</li>
+
+            <li>About</li>
+
+            <li>Contact</li>
+
+        </ul>
+
+    </nav>
+
+)
+
+ReactDOM.render(
+    navbar, 
+    document.getElementById("root")
+)
