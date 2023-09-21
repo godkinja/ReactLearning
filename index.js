fda2c298bbeff843329534b21b03719bd4238cf2
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-21 21:30:26 +0100

    Changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-ReactDOM.render(<h1>Hello, Everyone!</h1>, document.getElementById('root'))
+ReactDOM.render(<h1>Hello, Everyone!</h1>, document.getElementById('root')) // 1º param: o que será renderizado, 2º param: onde será renderizado
 
 ReactDOM.render(
-    <ul><li>Menu 1</li><li>Menu 2</li><li>Menu 3</li></ul>, 
+    <ul><li>Menu 1</li><li>Menu 2</li><li>Menu 3</li></ul>,
     document.querySelector('#root2')
 )
