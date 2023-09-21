d7463086b77d00719700effe7388b50362006afb
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-21 21:26:14 +0100

    First React Exercise

---
+++ index.js
@@ -0,0 +1,3 @@
+ReactDOM.render(<h1>Hello, Everyone!</h1>, document.getElementById('root'))
+
+ReactDOM.render(<ul><li>Menu 1</li><li>Menu 2</li><li> Menu 3</li></ul>, document.querySelector('#root2'))
