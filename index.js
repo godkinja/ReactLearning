449f8a9bf76396de0260e01b237324921d78eb4a
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-21 21:28:21 +0100

    Indentation

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 ReactDOM.render(<h1>Hello, Everyone!</h1>, document.getElementById('root'))
 
-ReactDOM.render(<ul><li>Menu 1</li><li>Menu 2</li><li> Menu 3</li></ul>, document.querySelector('#root2'))
+ReactDOM.render(
+    <ul><li>Menu 1</li><li>Menu 2</li><li>Menu 3</li></ul>, 
+    document.querySelector('#root2')
+)
