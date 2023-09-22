cadf7a5396e3bcb7c69bfc006beb4b8d24c157c9
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-22 23:02:09 +0100

    React Icons added

--- Digital-Card/src/components/Footer.jsx
+++ Digital-Card/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { BsLinkedin, BsGithub, BsTwitter, BsFacebook } from 'react-icons/bs'
 
 export default function Footer() {
 
@@ -9,13 +9,13 @@ export default function Footer() {
 
             <div className="footer">
 
-                <a href="https://www.linkedin.com/in/rafaraimundo/" target="_blank">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/rafaraimundo/" target="_blank"><BsLinkedin /></a>
 
-                <a href="https://https://github.com/godkinja" target="_blank">GitHub</a>
+                <a href="https://https://github.com/godkinja" target="_blank"><BsGithub /></a>
 
-                <a href="https://twitter.com/raimund0w" target="_blank">Twitter</a>
+                <a href="https://twitter.com/raimund0w" target="_blank"><BsTwitter /></a>
 
-                <a href="https://www.facebook.com/rafael.raimundo.7/" target="_blank">Facebook</a>
+                <a href="https://www.facebook.com/rafael.raimundo.7/" target="_blank"><BsFacebook /></a>
 
             </div>
 
