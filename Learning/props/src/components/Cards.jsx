7560e3b20ac5935b1755c88d8b08dbf232336691
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-27 05:12:07 +0100

    Cards Update

--- Learning/props/src/components/Cards.jsx
+++ Learning/props/src/components/Cards.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Cards(props) {
 
+    const {img, name, phone, email} = props
+
+    /*In case we declare the variables like Line 5, we don't need to call it as "props.img" and we can only call it img because it is already pointing to
+    the props that is recieving*/
+
     console.log(props)
 
     return (
@@ -9,7 +14,7 @@ export default function Cards(props) {
         
         <div className="contact-card">
 
-            <img src={props.img}/>
+            <img src={props.img}/> 
 
             <h3>{props.name}</h3>
 
@@ -35,3 +40,42 @@ export default function Cards(props) {
     )
 
 }
+
+/*
+
+1. What do props help us accomplish?
+
+Make a component more reusable.
+
+
+2. How do you pass a prop into a component?
+
+<MyAwesomeHeader title="???" />
+
+
+3. Can I pass a custom prop (e.g. `blahblahblah={true}`) to a native
+   DOM element? (e.g. <div blahblahblah={true}>) Why or why not?
+
+No, because the JSX we use to describe native DOM elements will
+be turned into REAL DOM elements by React. And real DOM elements
+only have the properties/attributes specified in the HTML specification.
+(Which doesn't include properties like `blahblahblah`)
+
+
+4. How do I receive props in a component?
+
+function Navbar(props) {
+    console.log(props.blahblahblah)
+    return (
+        <header>
+            ...
+        </header>
+    )
+}
+
+
+5. What data type is `props` when the component receives it?
+
+An object!
+
+*/
