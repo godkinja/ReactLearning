f79c325c31645e80bc649de8fc47fb6108a43bab
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-27 05:07:04 +0100

    Updates on Props + imports on cards

--- Learning/props/src/App.jsx
+++ Learning/props/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cards from './components/Cards.jsx'
 
 export default function App() {
 
   const firstName = 'John'
+
   const lastName = 'Doe'
 
   const date = new Date()
@@ -27,6 +30,7 @@ export default function App() {
 
   return (
     <>
+
       <h1>Hello {firstName} {lastName}!</h1>
 
       <h1>It is currently about {date.getHours() % 12} o'clock!</h1>
@@ -35,6 +39,44 @@ export default function App() {
 
       <h1>Good {timeOfDay}</h1>
 
+      <div className="contacts">
+
+        <Cards 
+          img = "mr-whiskerson.png"
+          name = "Mr. Whiskerson"
+          phone = "(212) 555-1234"
+          email = "mr.whiskaz@catnap.meow"
+        />
+
+        <Cards 
+        
+          img = "fluffykins.png"
+          name = "Fluffykins"
+          phone = "(212) 555-2345"
+          email = "fluff@me.com"
+        
+        />
+
+        <Cards 
+        
+          img = "felix.png"
+          name = "Felix"
+          phone = "(212) 555-4567"
+          email = "thecat@hotmail.com"      
+        
+        />
+
+        <Cards 
+        
+          img = "pumpkin.png"
+          name = "Pumpkin"
+          phone = "(0000) CAT KING"
+          email = "pumpkin@scrimba.com"
+        
+        
+        />
+
+      </div>
     </>
   )
 }
