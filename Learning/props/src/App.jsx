0f796753b21c0f0b09375d934061dfcd05040765
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-27 04:28:43 +0100

    Update hours

--- Learning/props/src/App.jsx
+++ Learning/props/src/App.jsx
@@ -7,11 +7,33 @@ export default function App() {
 
   const date = new Date()
 
+  const hours = date.getHours() % 12
+
+  let timeOfDay
+
+  if (hours < 12) {
+
+    timeOfDay = 'morning'
+
+  } else if (hours >= 12 && hours < 17) {
+
+    timeOfDay = 'afternoon'
+
+  } else {
+
+    timeOfDay = 'night'
+
+  }
+
   return (
     <>
       <h1>Hello {firstName} {lastName}!</h1>
 
       <h1>It is currently about {date.getHours() % 12} o'clock!</h1>
+
+      <h1>It is currently about {hours} o'clock!</h1>
+
+      <h1>Good {timeOfDay}</h1>
 
     </>
   )
