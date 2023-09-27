909c77b1ddfa4479497aa4e6396e632ea0ba6273
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-27 04:23:10 +0100

    Props learning

---
+++ Learning/props/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+
+  const firstName = 'John'
+  const lastName = 'Doe'
+
+  return (
+    <>
+      <h1>Hello {firstName} {lastName}!</h1>
+    </>
+  )
+}
