cbb6ab1ca60680b336cca7d159e3488884121ce0
godkinja <123703059+godkinja@users.noreply.github.com>
2023-09-27 04:24:49 +0100

    Clock adding

--- Learning/props/src/App.jsx
+++ Learning/props/src/App.jsx
@@ -5,9 +5,14 @@ export default function App() {
   const firstName = 'John'
   const lastName = 'Doe'
 
+  const date = new Date()
+
   return (
     <>
       <h1>Hello {firstName} {lastName}!</h1>
+
+      <h1>It is currently about {date.getHours() % 12} o'clock!</h1>
+
     </>
   )
 }
