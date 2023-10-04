792cc003a1f9d5df49c46d21b30819841e249b32
godkinja <123703059+godkinja@users.noreply.github.com>
2023-10-04 09:53:57 +0100

    Moved <Joke /> component out of the return

--- Learning/props/src/App.jsx
+++ Learning/props/src/App.jsx
@@ -105,61 +105,65 @@ export default function App() {
 
       <h1>Jokes</h1>
 
-      <Joke 
-          punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
-          isPun={false}
-          upvotes={10}
-          downvotes={5}
-          comments={[{author: "", body: "", title: ""}]}
-      />
-
-      <Joke 
-          setup="I got my daughter a fridge for her birthday." 
-          punchline="I can't wait to see her face light up when she opens it."
-          isPun={true}
-          upvotes={10}
-          downvotes={5}
-          comments={[{author: "", body: "", title: ""}]}
-      />
-      <Joke 
-          setup="How did the hacker escape the police?" 
-          punchline="He just ransomware!"
-          isPun={true}
-          upvotes={10}
-          downvotes={5}
-          comments={[{author: "", body: "", title: ""}]}
-      />
-      <Joke 
-          setup="Why don't pirates travel on mountain roads?" 
-          punchline="Scurvy." 
-          isPun={false} 
-          upvotes={10}
-          downvotes={5}
-          comments={[{author: "", body: "", title: ""}]}
-      />
-      <Joke 
-          setup="Why do bees stay in the hive in the winter?" 
-          punchline="Swarm."
-          isPun={true}
-          upvotes={10}
-          downvotes={5}
-          comments={[{author: "", body: "", title: ""}]}
-      />
-      <Joke 
-          setup="What's the best thing about Switzerland?" 
-          punchline="I don't know, but the flag is a big plus!"
-          isPun={false} 
-          upvotes={10}
-          downvotes={5}
-          comments={[{author: "", body: "", title: ""}]}
-      />
+      {jokeElements}
 
       <Arrays />
 
       {colors}
 
-      {jokeElements}
-
     </>
   )
 }
+
+/*
+
+  <Joke 
+      punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+      isPun={false}
+      upvotes={10}
+      downvotes={5}
+      comments={[{author: "", body: "", title: ""}]}
+  />
+
+  <Joke 
+      setup="I got my daughter a fridge for her birthday." 
+      punchline="I can't wait to see her face light up when she opens it."
+      isPun={true}
+      upvotes={10}
+      downvotes={5}
+      comments={[{author: "", body: "", title: ""}]}
+  />
+  <Joke 
+      setup="How did the hacker escape the police?" 
+      punchline="He just ransomware!"
+      isPun={true}
+      upvotes={10}
+      downvotes={5}
+      comments={[{author: "", body: "", title: ""}]}
+  />
+  <Joke 
+      setup="Why don't pirates travel on mountain roads?" 
+      punchline="Scurvy." 
+      isPun={false} 
+      upvotes={10}
+      downvotes={5}
+      comments={[{author: "", body: "", title: ""}]}
+  />
+  <Joke 
+      setup="Why do bees stay in the hive in the winter?" 
+      punchline="Swarm."
+      isPun={true}
+      upvotes={10}
+      downvotes={5}
+      comments={[{author: "", body: "", title: ""}]}
+  />
+  <Joke 
+      setup="What's the best thing about Switzerland?" 
+      punchline="I don't know, but the flag is a big plus!"
+      isPun={false} 
+      upvotes={10}
+      downvotes={5}
+      comments={[{author: "", body: "", title: ""}]}
+  />
+
+*/
