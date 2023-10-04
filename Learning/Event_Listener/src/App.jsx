a1b28dee00bd877ea2f98c779b024d71615af760
godkinja <123703059+godkinja@users.noreply.github.com>
2023-10-04 11:22:37 +0100

    EventListeners update

--- Learning/Event_Listener/src/App.jsx
+++ Learning/Event_Listener/src/App.jsx
@@ -1,14 +1,32 @@
 export default function App() {
 
+  function handleClick() {
+
+    console.log('Button clicked');
+
+  }
+
+  function imageHover() {
+
+    console.log('Image hovered');
+
+  }
+
+  function imageClick() {
+
+    console.log('Image clicked');
+
+  }
+
   return (
     <>
 
       <div className="container">
 
-          <img src="https://picsum.photos/640/360" />
+          <img src="https://picsum.photos/640/360" onMouseOver={imageHover} onClick={imageClick} />
 
-          <button>Click me</button>
-          
+          <button onClick={handleClick}>Click me</button>
+
       </div>
 
     </>
