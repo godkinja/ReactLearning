aff0c06b35531f6f087fc45c1b4da020a9969096
godkinja <123703059+godkinja@users.noreply.github.com>
2023-10-04 11:14:36 +0100

    Event Listeners Start

---
+++ Learning/Event_Listener/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+
+  return (
+    <>
+
+      <div className="container">
+
+          <img src="https://picsum.photos/640/360" />
+
+          <button>Click me</button>
+          
+      </div>
+
+    </>
+  )
+}
+
