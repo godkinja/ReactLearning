af2af4d2592f7f2302e40e7a816fc9ceccd2b9af
godkinja <123703059+godkinja@users.noreply.github.com>
2023-10-14 00:35:48 +0100

    Array Destructuring

---
+++ Learning/use_states/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+export default function App() {
+
+  /*function greeting(name){
+
+    const date = new Date();
+
+    const hours = date.getHours();
+
+    let timeOfDay;
+
+    if (hours >= 4 && hours < 12){
+
+      timeOfDay = "morning";
+
+    } else if (hours >= 12 && hours < 17){
+
+      timeOfDay = "afternoon";
+
+    } else if (hours >= 17 && hours < 20){
+
+      timeOfDay = "evening";
+
+    } else {
+
+      timeOfDay = "night";
+
+    }
+
+    return `Good ${timeOfDay} ${name}`;
+
+  }
+
+  console.log(greeting("John"))*/
+
+  const [isImportant, func] = useState("Yes"); // [value, function] 
+
+  console.log(isImportant);
+    
+  return (
+
+      <div className="state">
+
+          <h1 className="state--title">Is state important to know?</h1>
+
+          <div className="state--value">
+
+              <h1>{isImportant}</h1>
+
+          </div>
+
+      </div>
+
+  )
+
+}
+
