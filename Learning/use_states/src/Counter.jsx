5e3133a8e54af5b478722a705edeb3edca72571f
godkinja <123703059+godkinja@users.noreply.github.com>
2023-10-14 00:56:36 +0100

    Callback Function

--- Learning/use_states/src/Counter.jsx
+++ Learning/use_states/src/Counter.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from "react"
 
 export default function App() {
-    
+
     const [count, setCount] = useState(0)
 
-    function increment(){
-
-        setCount(count + 1)
-
-    }
+    /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
 
     function decrement(){
 
-        setCount(count - 1)
+        setCount(prevCount => prevCount - 1)
+
+    }
+
+    function increment(){
+
+        setCount(prevCount => prevCount +1)
 
     }
 
